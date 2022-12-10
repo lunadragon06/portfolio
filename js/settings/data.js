69f753cb9a245f4c6305c3b0bbb1299f40121b1d
Monika Lie <74667575+LunaDragon666@users.noreply.github.com>
2022-12-10 20:31:27 +0100

    Create data.js

---
+++ js/settings/data.js
@@ -0,0 +1,29 @@
+export const portfolio = [
+    {
+        name: "Torf's Gallery of Art",
+        year: "2022",
+        category: "Webdesign",
+        image: "../img/portfolio/project.jpg",
+        id: "1",
+        description: "A freelance project I have worked on through Noroff related to Project Exam 2, where project will be resumed during next year. This is a simple and minimalistic website built using React with JavaScript and SASS. The backend for development of Torf's Gallery is currently running on the remote server with Strapi, Heroku and Coludinary.",
+        github: "https://github.com/lunadragon06/torfs-art-gallery/tree/master",
+    },
+    {
+        name: "GitHub",
+        year: "2022",
+        category: "Application",
+        image: "../img/portfolio/project2.jpg",
+        id: "2",
+        description: "This project is linked to JS Frameworks CA from 2nd year and last semester of the Front-end Development program at Noroff Vocational School. I used one of my WordPress API which I have had since 1st year at Noroff Vocational School while working on this assignment and practiced Reactjs throughout this entire course.",
+        github: "https://github.com/lunadragon06/gamehub/tree/main",
+    },
+    {
+        name: "Betanya's Cosmetics",
+        year: "2022",
+        category: "UX | UI",
+        image: "../img/portfolio/project3.jpg",
+        id: "3",
+        description: "Fictional e-commerce website that belongs to Semester Project 2. The project theme is also chosen based on personal interest and curiousity for vegan and cruelty-free beauty products. The goal of this assignment was to use everything I learned and could so far in vanilla JavaScript from 3rd semester (2nd year) of Frontend Development study program.",
+        github: "https://github.com/LunaDragon666/gamehub",
+    }
+ ];
